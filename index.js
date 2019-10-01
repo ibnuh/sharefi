781acd8b8fd47940914be8ae929986e5dc29482a
Muhammad Ibnuh <ibnuhdev@gmail.com>
2019-10-01 07:50:40 +0700

    Refactor index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const path = require('path')
 const ip = require('ip')
 const qrcode = require('qrcode')
 
-const app = express()
-
 args.option('port', 'The port on which the app will be running', 9000).option('terminal', 'Ouput QRCode in terminal')
-
 const flags = args.parse(process.argv)
 
 const file = args.sub[0]
@@ -15,23 +12,23 @@ const absolutePath = path.resolve(file)
 const filename = path.basename(absolutePath)
 const ipAddress = ip.address()
 
-const url = `http://${ipAddress}:${flags.port}/${filename}`
-let qrBase64 = ''
+const url = `http://${ipAddress}:${flags.port}`
 
 if (flags.terminal) {
-    qrcode.toString(url, { type: 'terminal' }, function(err, url) {
+    qrcode.toString(url, { type: 'terminal' }, (err, url) => {
         console.log(url)
-    })
-} else {
-    qrcode.toDataURL(url, { width: 500 }, function(err, url) {
-        qrBase64 = url
     })
 }
 
+let qr = ''
+qrcode.toDataURL(url, { width: 500 }, (err, url) => {
+    qr = url
+})
+
+const app = express()
 app.get(`/${filename}`, (req, res) => res.sendFile(absolutePath))
 app.get('/qr', (req, res) => {
-    var img = Buffer.from(qrBase64.replace(/^data:image\/png;base64,/, ''), 'base64')
-
+    let img = Buffer.from(qr.replace(/^data:image\/png;base64,/, ''), 'base64')
     res.writeHead(200, {
         'Content-Type': 'image/png',
         'Content-Length': img.length,
@@ -40,6 +37,6 @@ app.get('/qr', (req, res) => {
 })
 
 app.listen(flags.port, () => {
-    console.log(`Sharing on ${url}`)
-    console.log(`QRCode available on http://${ipAddress}:${flags.port}/qr`)
+    console.log(`Sharing on ${url}/${filename}`)
+    console.log(`QRCode available on ${url}/qr`)
 })
